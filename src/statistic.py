0debf04803107b0a01cf6d0f7df972afb41b9f72
mia <viashipcosmos@gmail.com>
2025-03-27 21:22:00 +0800

    desktop_bk

---
+++ src/statistic.py
@@ -0,0 +1,11 @@
+import numpy as np
+with open("/home/mia/Repositories/Projects/2025-code-craft-preliminary-0313update/data/sample_practice.insrc/statistic.py") as f:
+    
+    for line in f:
+        datas = int(line.split())
+        if len(datas)==48:
+            tag += 1
+            write
+
+
+
