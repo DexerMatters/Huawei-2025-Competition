c3d94e6c922085d8acd85bf3214c3150df8d7062
mia <viashipcosmos@gmail.com>
2025-03-30 14:23:33 +0800

    fix merge

--- include/libs.hpp
+++ include/libs.hpp
@@ -1,8 +1,6 @@
 #ifndef LIBS_HPP
 #define LIBS_HPP
 
-// #define DEBUG_MODE
-
 #include <iostream>
 #include <vector>
 #include <array>
@@ -24,17 +22,14 @@ constexpr auto FRE_PER_SLICING = (1800);
 constexpr auto EXTRA_TIME = (105);
 constexpr iarray<128> READING_COSTS =
 {
-    0, 64, 52, 42, 34, 28, 23, 19, 16, 16, 16, 16,
-    16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-    16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-    16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+    0, 64, 16, 13, 11, 9, 8, 7, 6, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
 };
 
 #define rep_char(c, n) std::string(n, c)
 
-#if defined(DEBUG_MODE)
-#define scanf_s debug_scanf
-#elif defined(__linux__) || defined(__APPLE__)
+#if defined(__linux__) || defined(__APPLE__)
 #define scanf_s scanf
 #endif
 
@@ -48,7 +43,7 @@ struct Request
 struct Object
 {
     int replica[REP_NUM + 1];
-    ivector unit[REP_NUM + 1];
+    int* unit[REP_NUM + 1];
     int size;
     bool is_delete;
 };
@@ -96,6 +91,11 @@ extern std::vector<ivector> fre_write;
 
 extern std::vector<ivector> fre_read;
 
+extern std::vector<int> tag_sort_by_busy_time;
+
+extern std::vector<int> sizes_sorted_by_tag;
+
+
 /****************************************************************************************
  * Interaction functions
  ****************************************************************************************/
@@ -103,8 +103,6 @@ extern std::vector<ivector> fre_read;
 void request_initalization();
 
 void request_freq_init();
-
-void skip_freq_init();
 
 // Request a timestamp
 void request_timestamp();
@@ -140,12 +138,10 @@ void release_unit(int object_id);
 
 void make_read_request(int req_id, int object_id);
 
-int find_closest_request(int disk_id);
-
 std::pair<std::vector<std::string>, ivector> read();
 
 /****************************************************************************************
- * Auxiliary functions
+ * Auxiliary fuctions
  ****************************************************************************************/
 
 void scan_numbers(ivector& vector);
@@ -153,8 +149,6 @@ void scan_numbers(ivector& vector);
 void reset_tokens();
 
 int calculate_distance(int point, int dest);
-
-int calculate_distance(int disk_id, int point, int object_id);
 
 bool is_farther_than(int dest1, int dest2);
 
@@ -164,12 +158,137 @@ void delete_recorded_request(int req_id);
 
 bool process_request(int disk_id, int req_id);
 
-int which_replica(int disk_id, int object_id);
+std::vector<int> getMaxIndices(const std::vector<std::vector<int>>& matrix);
 
-/****************************************************************************************
- * Debug functions
- ****************************************************************************************/
+std::vector<int> getRowSums(const std::vector<std::vector<int>>& matrix);
 
-int debug_scanf(const char* format, ...);
+/****************************
+ * middleware functions
+**************************/
+template <size_t REP_NUM> int count_zero(const iarray<REP_NUM> &vec);
+struct disk_group_unit_info;
+struct disk_unit_info;
+
+class DiskGroup
+{
+public:
+    /*
+    * Constructor for DiskGroup
+    * @param tags_sorted_by_busy_time: Vector of tags sorted by busy time
+    * @param sizes_sorted_by_tag: Vector of sizes sorted by tag
+    * @param token_size: Size of each token 
+    * @param disk_amount: Number of disks
+    * @param disk_size: Size of each disk
+    * @param tag_amount: Number of tags
+    */
+   DiskGroup();
+    DiskGroup(
+        // tag level info, for disk-group-to-disk
+        const ivector &tags_sorted_by_busy_time,
+        // index begin with **1**
+        const ivector &sizes_sorted_by_tag,
+        // disk level info, for disk-to-disk
+        int repica_block_size,
+        // index begin with **1**
+        int disk_amount,
+        // index begin with **1**
+        int disk_size,
+        // index begin with **0**   
+        int tag_amount
+        );
+    Object write_to_group(int object_id, int object_tag, int object_size);
+    void set(// tag level info, for disk-group-to-disk
+        const ivector &tags_sorted_by_busy_time,
+        // index begin with **1**
+        const ivector &sizes_sorted_by_tag,
+        // disk level info, for disk-to-disk
+        int repica_block_size,
+        // index begin with **1**
+        int disk_amount,
+        // index begin with **1**
+        int disk_size,
+        // index begin with **0**   
+        int tag_amount
+        );
+    private:
+
+    /*****************
+     * member variables
+     ******************/
+    // `is_fallback_available`
+    // layer: external
+    bool is_fallback_available;
+
+    // `fallback_disk_start_index`
+    // layer: external::phyical disk
+    int fallback_disk_start_index;
+
+    // `fallback_disk_size`
+    // layer: external::phyical disk
+    int fallback_disk_size;
+
+
+    // `tag_amount`
+    // layer: external::tag
+    int tag_amount; 
+    int repica_block_size; 
+    int tag_block_size; 
+
+    // `tag_block_limit_amount_for_disk`
+    // layer: external::phyical disk
+    int disk_amount;
+    int disk_size; 
+    int tag_block_limit_amount_for_disk;
+
+
+    // `disk_group_amount`
+    // layer: disk group
+    int disk_group_amount;
+    // `disk_group_id_head_disk_mapping_list`
+    // layer: disk group - disk
+    // size: [disk_group_amount]
+    // shape[0] begins with **1**
+    ivector disk_group_id_head_disk_mapping_list;
+    /*
+    * `tag_block_start_index_sorted_by_tag_for_each_disk`
+    * layer: tag block
+    * size: [disk_group_amount][tag_amount]
+    * shape[0] begins with **1**, shape[1] begins with **1**
+    */
+   std::vector<ivector> tag_block_start_index_sorted_by_tag_for_each_disk;
+
+   /*
+   * `tag_block_amount_sorted_by_tag_for_each_disk`
+   * layer: tag block
+   * size: [disk_group_amount][tag_amount]
+   * shape[0] begins with **1**, shape[1] begins with **1**
+   */
+   std::vector<ivector> tag_block_amount_sorted_by_tag_for_each_disk; 
+   /* ****************************
+   * operation functions for disk group-disk layer
+   ******************************** */
+    // * @name: `read_disk_group_unit_usage`
+    // * layer: disk group-disk
+    // * int -> int -> int
+    // * return: how many non-zero ints are in the disk group unit
+    // * @params:
+    // * disk_group_id: the id of the disk group
+    // * trac_unit_id: the id of the unit
+    // * @return: how many zero ints are in the disk group unit
+    int read_disk_group_unit_usage(int disk_group_id, int unit_id);
+    disk_unit_info read_disk_group_position(int disk_group_id, int unit_id, int blind_unit_amount);
+    disk_unit_info write_disk_group_position(int disk_group_id, int unit_id, int blind_unit_amount, int value);
+    /* ****************************
+    * operation functions for object-disk group layer
+    ******************************** */
+   disk_group_unit_info find_available_disk_space(int object_id, int object_tag, int object_size);
+   disk_group_unit_info read_2unit(int disk_group_id, int unit_id, int object_size);
+   //********************************************* *//
+   iarray<REP_NUM + 1> alloc_replica_disk_ids_in_fallback(int object_id, int object_tag);
+};
+
+
+// Disk group manager
+extern DiskGroup disk_group_maneger;
 
 #endif // !LIBS_HPP
--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ int main(int, char**) {
     //skip_freq_init();
 
     for (int t = 1; t <= T + EXTRA_TIME; t++) {
+        std::cerr << "Time: " << t << '\n';
         request_timestamp();
+        std::cerr << "Time: " << t << " " << "del" << '\n';
         request_delete();
+        std::cerr << "Time: " << t << " " << "write" << '\n';
         request_write();
+        std::cerr << "Time: " << t << " " << "read" << '\n';
         request_read();
     }
 
